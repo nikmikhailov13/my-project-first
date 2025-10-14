bbb7b56f1f96feb61d052a741f7325a0766d1172
Nikita Mykhailov <nikmikhailov13@gmail.com>
2025-10-14 17:32:42 +0200

    add sample code to main method

---
+++ src/main/java/lesson_6_variables/Main.java
@@ -0,0 +1,21 @@
+package lesson_6_variables;
+
+public class Main {
+
+    static void main() {
+
+        int a = 10;
+        int b = 20;
+
+        int c = a - b;
+
+        char d = '!';
+
+        boolean e = true;
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+
+}
